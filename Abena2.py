760f59688e0ab3037364211c9d58f0536107f92a
Abena Rapha <mushabena18@gmail.com>
2025-01-03 00:05:05 +0000

    initial commit

--- Abena2.py
+++ Abena2.py
@@ -1,11 +1,21 @@
-print("Helllo World")
-if 5>2:
-  print("Five is greater than two!")
-x = 5
-y ="Hello world" 
-  print(x)
-  print(y)
+print("Hello World")
 
+new = "Hello World"
+print(new)
+
+first_name = 'Musharafa'
+last_name = 'Moro'
+full_name = first_name + '' + last_name
+print(full_name)
+
+bikes = ['Red', 'Green', 'Blue']
+
+first_bike = bikes[0]
+second_bike = bikes[1]
+last_bike = bikes[-1]
+
+for bike in bikes:
+    print(first_bike,second_bike, last_bike)
 
 
 
